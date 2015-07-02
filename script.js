d9e29c5a38bb1fb16dc31deae9b1253c6bd49f4e
Ryohei Ikegami <iofg2100@gmail.com>
2015-07-02 11:53:07 +0900

    Add menu

--- script.js
+++ script.js
@@ -10,3 +10,26 @@ new Vue({
     marked: marked
   }
 });
+
+var remote = require('remote');
+var Menu = remote.require('menu');
+var template = [
+  {
+    label: 'Markdown Editor',
+  },
+  {
+    label: 'File',
+    submenu: [
+      {
+        label: "Open",
+        click: function () {}
+      },
+      {
+        label: "Save",
+        click: function () {}
+      },
+    ]
+  }
+];
+menu = Menu.buildFromTemplate(template);
+Menu.setApplicationMenu(menu);
